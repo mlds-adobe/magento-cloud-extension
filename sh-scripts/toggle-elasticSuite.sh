a87a61294ebc6c98220bc4c9a60450259aa97645
mlds-adobe <52993191+mlds-adobe@users.noreply.github.com>
2022-11-16 13:20:57 +0100

    Update toggle-elasticSuite.sh

--- sh-scripts/toggle-elasticSuite.sh
+++ sh-scripts/toggle-elasticSuite.sh
@@ -15,7 +15,7 @@ service_file="$tmp_git_dir"/.magento/services.yaml
 
 perl -i -pe "s/engine: elasticsearch7/engine: elasticsuite/" "$env_file"
 perl -i -pe "s/type: opensearch:1.2/type: opensearch:1.2\r\n    configuration:\r\n        plugins:\r\n            - analysis-icu\r\n            - analysis-phonetic/g" "$service_file"
-composer require smile/elasticsuite
+composer require smile/elasticsuite ~2.10.12
 #perl -i -pe "s/\"paradoxlabs\/tokenbase\": \"4.5.1.x-dev\"/\"paradoxlabs\/tokenbase\": \"4.5.1.x-dev\", \r\n\"smile\/elasticsuite\": \"\^2.10\"/" "$service_file"
 echo "        CONFIG__DEFAULT__SMILE_ELASTICSUITE_CORE_BASE_SETTINGS__ES_CLIENT__SERVERS: 'opensearch.internal'" >> $app_file
 
