af40460b1a0ea579e10c833be07e0b4fdcea6c9f
mlds-adobe <52993191+mlds-adobe@users.noreply.github.com>
2022-11-17 08:18:16 +0100

    Update toggle-elasticSuite.sh

--- sh-scripts/toggle-elasticSuite.sh
+++ sh-scripts/toggle-elasticSuite.sh
@@ -15,6 +15,12 @@ service_file="$tmp_git_dir"/.magento/services.yaml
 
 perl -i -pe "s/engine: elasticsearch7/engine: elasticsuite/" "$env_file"
 perl -i -pe "s/type: opensearch:1.2/type: opensearch:1.2\r\n    configuration:\r\n        plugins:\r\n            - analysis-icu\r\n            - analysis-phonetic/g" "$service_file"
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
 composer require smile/elasticsuite ~2.10.12
 #perl -i -pe "s/\"paradoxlabs\/tokenbase\": \"4.5.1.x-dev\"/\"paradoxlabs\/tokenbase\": \"4.5.1.x-dev\", \r\n\"smile\/elasticsuite\": \"\^2.10\"/" "$service_file"
 echo "        CONFIG__DEFAULT__SMILE_ELASTICSUITE_CORE_BASE_SETTINGS__ES_CLIENT__SERVERS: 'opensearch.internal'" >> $app_file
